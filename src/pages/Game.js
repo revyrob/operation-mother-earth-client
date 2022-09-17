63427041724a14b60d4d5f3ab4df03a9f4e97728
revyrob <kayle.robson@gmail.com>
2022-09-17 13:39:16 -0700

    worked on game component

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,4 +1,19 @@
+import game from "../assets/icons/game-icon.svg";
+import namePic from "../assets/images/mother-name.png";
+import TitleHeader from "../components/TitleHeader/TitleHeader";
+import InputName from "../components/InputName/InputName";
+
 function Game() {
-  return;
+  return (
+    <section className="game">
+      <TitleHeader
+        img={game}
+        alt={"game icon"}
+        text={"Welcome to the Operation Mother Earth Game"}
+      />
+      <img className="game__img" src={namePic} alt="mother earth" />
+      <InputName />
+    </section>
+  );
 }
 export default Game;
