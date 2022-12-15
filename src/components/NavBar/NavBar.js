554770027ca1f507846495c4ead919305322584b
revyrob <kayle.robson@gmail.com>
2022-12-15 12:08:02 -0800

    added nav link and coloring when nav is active

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -3,37 +3,69 @@ import education from "../../assets/icons/books-icon.svg";
 import game from "../../assets/icons/game-icon.svg";
 import recycling from "../../assets/icons/recycling-icon.svg";
 import "./NavBar.scss";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 function NavBar() {
   return (
     <section className="nav">
       <div className="phantomStyle" />
       <div className="nav__style">
-        <Link to="/">
-          <div className="nav__div">
-            <img src={home} alt="home button" className="nav__img" />
-            <p className="nav__subtitle">Home</p>
+        <NavLink to="/" className="nav__link">
+          <div
+            className={(isActive) =>
+              "nav__div" + (!isActive ? "unselected" : "")
+            }
+          >
+            <div className="nav__div">
+              <img src={home} alt="home button" className="nav__img" />
+              <p className="nav__subtitle">Home</p>
+            </div>
           </div>
-        </Link>
-        <Link to="/education">
-          <div className="nav__div">
-            <img src={education} alt="education button" className="nav__img" />
-            <p className="nav__subtitle">Education</p>
+        </NavLink>
+        <NavLink to="/education" className="nav__link">
+          <div
+            className={(isActive) =>
+              "nav__div" + (!isActive ? " unselected" : "")
+            }
+          >
+            <div className="nav__div">
+              <img
+                src={education}
+                alt="education button"
+                className="nav__img"
+              />
+              <p className="nav__subtitle">Education</p>
+            </div>
           </div>
-        </Link>
-        <Link to="/game">
-          <div className="nav__div">
-            <img src={game} alt="game button" className="nav__img" />
-            <p className="nav__subtitle">Game</p>
+        </NavLink>
+        <NavLink to="/game" className="nav__link">
+          <div
+            className={(isActive) =>
+              "nav__div" + (!isActive ? " unselected" : "")
+            }
+          >
+            <div className="nav__div">
+              <img src={game} alt="game button" className="nav__img" />
+              <p className="nav__subtitle">Game</p>
+            </div>
           </div>
-        </Link>
-        <Link to="/recycling">
-          <div className="nav__div">
-            <img src={recycling} alt="recycling button" className="nav__img" />
-            <p className="nav__subtitle">Centers</p>
+        </NavLink>
+        <NavLink to="/recycling" className="nav__link">
+          <div
+            className={(isActive) =>
+              "nav__div" + (!isActive ? " unselected" : "")
+            }
+          >
+            <div className="nav__div">
+              <img
+                src={recycling}
+                alt="recycling button"
+                className="nav__img"
+              />
+              <p className="nav__subtitle">Centers</p>
+            </div>
           </div>
-        </Link>
+        </NavLink>
       </div>
     </section>
   );
